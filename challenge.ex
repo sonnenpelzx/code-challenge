09cc2d9f660273786f82251740cd4b7d33a9ce07
Tim <sonnenpelzxx@gmail.com>
2023-06-02 13:22:18 +0200

    Create challenge.ex

---
+++ challenge.ex
@@ -0,0 +1,15 @@
+defmodule Challenge do
+  def test1() do
+    [[0, 0, 1], [1, 0, 1]]
+  end
+  def test2() do
+    [[1,1], [1,1]]
+  end
+  def getHitProbability([], ships, fields) do
+    ships/fields
+  end
+  def getHitProbability([list | rest], ships, fields) do
+    {s,  f} = List.foldl(list, {0, 0}, fn x, {s, f} -> {x + s, f + 1} end)
+    getHitProbability(rest, ships + s, fields + f)
+  end
+end
